9e6c5735060ec822ed57fe6736a28d6a4f59f6df
khagendra62 <khagendraawasthi47@gmail.com>
2025-11-19 12:23:30 +0545

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-3xl text-orange-500 bg-gray-200 font-black py-3 text-center justify-center align-middle">
+      Password Generator
+    </div>
+  );
+};
+
+export default App;
